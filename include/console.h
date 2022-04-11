dad369006d5929e84c97f4b939576daf950002ec
Cloud User <ec2-user@ip-172-31-93-24.ec2.internal>
2022-04-11 00:36:46 +0000

    more changes

--- include/console.h
+++ include/console.h
@@ -1,6 +1,10 @@
 #ifndef _CONSOLE_H
 #define _CONSOLE_H
 
+static char* const VGA_MEMORY;
+static int VGA_WIDTH;
+static int VGA_HEIGHT;
+
 void print_character(char);
 void print_string(char*);
 void print_line(char*);
